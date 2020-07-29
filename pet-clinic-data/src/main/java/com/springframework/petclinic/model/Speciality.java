001b3d6b65db015c15d7dae147fc0bc0aa8d5529
Markian Rutkowskyj <i-keep-laughing@myself.com>
2020-07-29 21:01:53 +0100

    Coverted Vets class to JPA Enitty . Closes #36

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
@@ -1,7 +1,16 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    //inherits its i.d from base entity
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
